9da5c169fcc454b798ab0e8202004e57a37d4aad
yuya523 <kick.kun0523@gmail.com>
2025-07-12 11:48:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch('data/products.json')
+fetch('/ec-site/data/products.json')
   .then(response => response.json())
   .then(products => {
     const container = document.getElementById('product-list');
